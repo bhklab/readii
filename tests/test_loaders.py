bef39f731363d1693f859ffbcdd6686540c1e98d
Katy Scott <k.l.scott16@gmail.com>
2023-11-23 10:31:14 -0500

    test: add unit test for loadDicomSITK

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,2 +1,6 @@
 from area_bk.loaders import *
 
+def test_loadDicomSITK():
+    """Test loading DICOM from directory."""
+    actual = loadDicomSITK("tests/R01-001/09-06-1990-NA-CT_CHEST_ABD_PELVIS_WITH_CON-98785/3.000000-THORAX_1.0_B45f-95741")
+    assert type(actual) == sitk.Image
