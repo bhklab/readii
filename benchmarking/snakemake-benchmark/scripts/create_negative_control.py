540276050df112ddb679a50ca5ddafb927d95b4f
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-04-17 11:00:24 -0400

    test: benchmarking works using snakemake==7.32.4, only shows time and not memory usage though

--- benchmarking/snakemake-benchmark/scripts/create_negative_control.py
+++ benchmarking/snakemake-benchmark/scripts/create_negative_control.py
@@ -1,29 +1,28 @@
+from email.mime import base
 from readii.loaders import loadDicomSITK, loadSegmentation
-from readii.image_processing import flattenImage, alignImages, displayImageSlice, displayCTSegOverlay
+
 from readii.negative_controls import (
-    applyNegativeControl,
-    shuffleImage, 
-    makeRandomImage,
-    makeRandomRoi,
-    shuffleROI,
-    makeRandomNonRoi,
-    shuffleNonROI,
-    randomizeImageFromDistribtutionSampling,
-    makeRandomFromRoiDistribution,
-    makeRandomNonRoiFromDistribution,
+    applyNegativeControl
 )
 
+import SimpleITK as sitk
 
 ct_img_dir = snakemake.input["ct_img_dir"]
 seg_dcm_path = snakemake.input["seg_dcm_path"]
 
 negative_control = snakemake.params["negative_control"]
 roiLabel = snakemake.params["roiLabel"]
+modality = snakemake.params["modality"]
+
 
 result = applyNegativeControl(
     nc_type = negative_control,
-    baseImage = loadDicomSITK(ct_img_dir),
-    roiImage = loadSegmentation(seg_dcm_path),
+    baseImage = loadDicomSITK(imgDirPath=ct_img_dir),
+    baseROI = loadSegmentation(
+        segImagePath=seg_dcm_path, 
+        modality = modality, 
+        baseImageDirPath = ct_img_dir
+        )['Tumor_c40'],
     roiLabel = roiLabel,
 )
 
