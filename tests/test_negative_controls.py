c083878e86f8196ccbf2cf15eecb8b6c01f42754
Katy Scott <k.l.scott16@gmail.com>
2024-05-16 11:01:17 -0400

    test(negative_controls): added tests for no roiLabel input for ROI and non-ROI negative control functions

--- tests/test_negative_controls.py
+++ tests/test_negative_controls.py
@@ -307,3 +307,183 @@ def test_makeRandomNonRoiFromDistribution(nsclcCropped, randomSeed):
         "Random seed is not working for randomized from distribution non-ROI image. Random seed should be 10."
     assert randomized_non_roi_pixels[7,18,11] == -1, \
         "ROI is getting randomized when it shouldn't."
+    
+
+def test_noROILabel_shuffleROI(nsclcCropped, randomSeed):
+    " Test passing no roiLabel to shuffleROI"
+
+    croppedCT, croppedROI, _ = nsclcCropped
+
+    shuffled_roi_image = shuffleROI(croppedCT, croppedROI, randomSeed=randomSeed)
+
+    original_pixels = sitk.GetArrayFromImage(croppedCT)
+    shuffled_roi_pixels = sitk.GetArrayFromImage(shuffled_roi_image)
+
+    assert croppedCT.GetSize() == shuffled_roi_image.GetSize(), \
+        "Shuffled image size not same as input image"
+    assert croppedCT.GetSpacing() == shuffled_roi_image.GetSpacing(), \
+        "Shuffled image spacing not same as input image"
+    assert croppedCT.GetOrigin() == shuffled_roi_image.GetOrigin(), \
+        "Shuffled image origin not same as input image"
+    assert isinstance(shuffled_roi_image, sitk.Image), \
+        "Returned object is not a sitk.Image"
+    assert not np.array_equal(original_pixels, shuffled_roi_pixels), \
+        "No voxel values are being shuffled."
+    assert np.array_equal(np.sort(original_pixels.flatten()),
+                          np.sort(shuffled_roi_pixels.flatten())), \
+        "Shuffled pixel values in ROI are different"
+    assert shuffled_roi_pixels[0,0,0] == -740, \
+        "Voxel outside the ROI is being shuffled. Should just be the ROI voxels."
+    assert shuffled_roi_pixels[7,18,11] == -100, \
+        "Random seed is not working for shuffled ROI image, centre pixel has wrong value. Random seed should be 10."
+    
+
+def test_noROILabel_randomROI(nsclcCropped, randomSeed):
+    " Test passing no roiLabel to makeRandomROI"
+
+    croppedCT, croppedROI, _ = nsclcCropped
+
+    randomized_roi_image = makeRandomRoi(croppedCT, croppedROI, randomSeed=randomSeed)
+    original_arr_image = sitk.GetArrayFromImage(croppedCT)
+    minVoxelVal, maxVoxelVal = np.min(original_arr_image), np.max(original_arr_image)
+
+    randomized_arr_image = sitk.GetArrayFromImage(randomized_roi_image)
+
+    assert croppedCT.GetSize() == randomized_roi_image.GetSize(), \
+        "Randomized image size not same as input image"
+    assert croppedCT.GetSpacing() == randomized_roi_image.GetSpacing(), \
+        "Randomized image spacing not same as input image"
+    assert croppedCT.GetOrigin() == randomized_roi_image.GetOrigin(), \
+        "Randomized image origin not same as input image"
+    assert isinstance(randomized_roi_image, sitk.Image), \
+        "Returned object is not a sitk.Image"
+    assert not np.array_equal(original_arr_image, randomized_roi_image), \
+        "No voxel values have been changed to random."
+    assert randomized_arr_image.max() <= maxVoxelVal and randomized_arr_image.min() >= minVoxelVal, \
+        "Pixel values are not within the expected range"
+    assert randomized_arr_image[0,0,0] == -740, \
+        "Voxel outside the ROI is being randomized. Should just be the ROI voxels."
+    assert randomized_arr_image[7,18,11] == -400, \
+        "Random seed is not working for randomized ROI image, centre pixel has wrong value. Random seed should be 10."
+
+
+def test_noROILabel_randomROIFromDist(nsclcCropped, randomSeed):
+    " Test passing no roiLabel to makeRandomFromRoiDistribution"
+
+    croppedCT, croppedROI, _ = nsclcCropped
+
+    randomized_roi_image = makeRandomFromRoiDistribution(croppedCT, croppedROI, randomSeed=randomSeed)
+    original_arr_image = sitk.GetArrayFromImage(croppedCT)
+
+    randomized_roi_arr_image = sitk.GetArrayFromImage(randomized_roi_image)
+
+    assert croppedCT.GetSize() == randomized_roi_image.GetSize(), \
+        "Randomized ROI Sampled image size not same as input image"
+    assert croppedCT.GetSpacing() == randomized_roi_image.GetSpacing(), \
+        "Randomized ROI Sampled image spacing not same as input image"
+    assert croppedCT.GetOrigin() == randomized_roi_image.GetOrigin(), \
+        "Randomized ROI Sampled image origin not same as input image"
+    assert isinstance(randomized_roi_image, sitk.Image), \
+        "Returned object is not a sitk.Image"
+    assert not np.array_equal(original_arr_image, randomized_roi_arr_image), \
+        "No voxel values have been changed to random."
+    assert np.all(np.isin(randomized_roi_arr_image.flatten(), original_arr_image.flatten())), \
+        "Retuned object has values not sampled from original image"
+    assert randomized_roi_arr_image[0,0,0] == -740, \
+        "Voxel outside the ROI is being randomized. Should just be the ROI voxels."
+    assert randomized_roi_arr_image[7,18,11] == -5, \
+        "Random seed is not working for randomized from distribution ROI image, centre pixel has wrong value. Random seed should be 10."
+    
+
+def test_noROILabel_shuffleNonROI(nsclcCropped, randomSeed):
+    " Test passing no roiLabel to shuffleNonROI"
+
+    croppedCT, croppedROI, _ = nsclcCropped
+
+    shuffled_non_roi_image = shuffleNonROI(croppedCT, croppedROI, randomSeed=randomSeed)
+
+    original_pixels = sitk.GetArrayFromImage(croppedCT)
+    shuffled_non_roi_pixels = sitk.GetArrayFromImage(shuffled_non_roi_image)
+
+    assert croppedCT.GetSize() == shuffled_non_roi_image.GetSize(), \
+        "Shuffled image size not same as input image"
+    assert croppedCT.GetSpacing() == shuffled_non_roi_image.GetSpacing(), \
+        "Shuffled image spacing not same as input image"
+    assert croppedCT.GetOrigin() == shuffled_non_roi_image.GetOrigin(), \
+        "Shuffled image origin not same as input image"
+    assert isinstance(shuffled_non_roi_image, sitk.Image), \
+        "Returned object is not a sitk.Image"
+    assert not np.array_equal(original_pixels, shuffled_non_roi_pixels), \
+        "Shuffled image has different pixel values than original image. Should just be original pixels rearranged."
+    assert np.array_equal(np.sort(original_pixels.flatten()),
+                          np.sort(shuffled_non_roi_pixels.flatten())), \
+        "Shuffled pixel values outside ROI are different"
+    assert shuffled_non_roi_pixels[0,0,0] == 54, \
+        "Random seed is not working for shuffled non-ROI image, first voxel has wrong shuffle value. Random seed should be 10."
+    assert shuffled_non_roi_pixels[-1,-1,-1] == -617, \
+        "Random seed is not working for shuffled non-ROI image, last voxel has wrong shuffle value. Random seed should be 10."
+    assert shuffled_non_roi_pixels[7,18,11] == -1, \
+        "ROI is getting shuffled when it shouldn't."
+    
+    
+def test_noROILabel_randomNonROI(nsclcCropped, randomSeed):
+    " Test passing no roiLabel to makeRandomNonROI"
+
+    croppedCT, croppedROI, _ = nsclcCropped
+
+    randomized_non_roi_image = makeRandomNonRoi(croppedCT, croppedROI, randomSeed=randomSeed)
+
+    original_pixels = sitk.GetArrayFromImage(croppedCT)
+    randomized_non_roi_pixels = sitk.GetArrayFromImage(randomized_non_roi_image)
+
+    # Make ROI pixels = NaN to help check of min and max values in non-ROI pixels
+    original_roi_pixels = sitk.GetArrayFromImage(croppedROI)
+    roiMask = np.where(original_roi_pixels > 0, np.NaN, 1)
+    masked_randomized_non_roi_pixels = randomized_non_roi_pixels * roiMask
+    
+    assert croppedCT.GetSize() == randomized_non_roi_image.GetSize(), \
+        "Randomized image size not same as input image"
+    assert croppedCT.GetSpacing() == randomized_non_roi_image.GetSpacing(), \
+        "Randomized image spacing not same as input image"
+    assert croppedCT.GetOrigin() == randomized_non_roi_image.GetOrigin(), \
+        "Randomized image origin not same as input image"
+    assert isinstance(randomized_non_roi_image, sitk.Image), \
+        "Returned object is not a sitk.Image"
+    assert np.nanmax(masked_randomized_non_roi_pixels) <= np.max(original_pixels) and np.nanmin(masked_randomized_non_roi_pixels) >= np.min(original_pixels), \
+        "Pixel values outside ROI are not within the expected range"
+    assert randomized_non_roi_pixels[0,0,0] == -124, \
+        "Random seed is not working for randomized non-ROI image, first voxel has wrong random value. Random seed should be 10."
+    assert randomized_non_roi_pixels[-1,-1,-1] == 123, \
+        "Random seed is not working for randomized non-ROI image, last voxel has wrong random value. Random seed should be 10."
+    assert randomized_non_roi_pixels[7,18,11] == -1, \
+        "ROI is getting randomized when it shouldn't."
+    
+
+def test_noROILabel_randomNonROIFromDist(nsclcCropped, randomSeed):
+    " Test passing no roiLabel to makeRandomNonRoiFromDistribution"
+
+    croppedCT, croppedROI, _ = nsclcCropped
+
+    randomized_non_roi_image = makeRandomNonRoiFromDistribution(croppedCT, croppedROI, randomSeed=randomSeed)
+
+    original_pixels = sitk.GetArrayFromImage(croppedCT)
+    randomized_non_roi_pixels = sitk.GetArrayFromImage(randomized_non_roi_image)
+
+    assert croppedCT.GetSize() == randomized_non_roi_image.GetSize(), \
+        "Randomized image size not same as input image"
+    assert croppedCT.GetSpacing() == randomized_non_roi_image.GetSpacing(), \
+        "Randomized image spacing not same as input image"
+    assert croppedCT.GetOrigin() == randomized_non_roi_image.GetOrigin(), \
+        "Randomized image origin not same as input image"
+    assert isinstance(randomized_non_roi_image, sitk.Image), \
+        "Returned object is not a sitk.Image"
+    assert not np.array_equal(original_pixels, randomized_non_roi_pixels), \
+        "No voxel values have been changed to random."
+    assert np.all(np.isin(randomized_non_roi_pixels, original_pixels)), \
+        "Retuned object has values outside ROI not sampled from original image"
+    assert randomized_non_roi_pixels[0,0,0] == -709, \
+        "Random seed is not working for randomized from distribution non-ROI image. Random seed should be 10."
+    assert randomized_non_roi_pixels[-1,-1,-1] == -830, \
+        "Random seed is not working for randomized from distribution non-ROI image. Random seed should be 10."
+    assert randomized_non_roi_pixels[7,18,11] == -1, \
+        "ROI is getting randomized when it shouldn't."
