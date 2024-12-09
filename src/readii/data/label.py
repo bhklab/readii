1e89c177818185f9d549a3dfdc976fe6a2e51e24
Katy Scott <k.l.scott16@gmail.com>
2024-12-09 15:53:53 -0500

    feat: add function to set up a time outcome column for survival prediction

--- src/readii/data/label.py
+++ src/readii/data/label.py
@@ -1,5 +1,6 @@
 import re
 from pandas import DataFrame
+import numpy as np
 
 def getPatientIdentifierLabel(dataframe_to_search:DataFrame) -> str:
     """Function to find a column in a dataframe that contains some form of patient ID or case ID (case-insensitive). 
@@ -55,10 +56,56 @@ def convertDaysToYears(dataframe_with_outcome:DataFrame,
         Dataframe with a copy of the specified time column converted to years.
     """
 
-    print(f"Converting time column {time_column_label} from days to years, using divide_by={divide_by}.")
-
+    # Set up the new column name for the converted time column
     years_column_label = time_column_label + "_years"
     # Make a copy of the time column with the values converted from days to years and add suffic _years to the column name
     dataframe_with_outcome[years_column_label] = dataframe_with_outcome[time_column_label] / divide_by
 
     return dataframe_with_outcome
+
+
+
+def timeOutcomeColumnSetup(dataframe_with_outcome:DataFrame,
+                            outcome_column_label:str,
+                            standard_column_label:str,
+                            convert_to_years:bool = False,
+                            ):
+    """ Function to set up a time outcome column in a dataframe. Makes a copy of the specified outcome column with a standardized column name and converts it to years if specified.
+
+    Parameters
+    ----------
+    dataframe_with_outcome : DataFrame
+        Dataframe containing the outcome column to convert.
+    outcome_column_label : str
+        Label for the outcome column to convert in the dataframe.
+    standard_column_label : str
+        Name of the column to save the standardized outcome column as.
+    convert_to_years : bool, optional
+        Whether to convert the time column to years. The default is False.
+
+    Returns
+    -------
+    dataframe_with_outcome : DataFrame
+        Dataframe with a copy of the specified outcome column converted to years.    
+    """
+
+    # Check if the outcome column is numeric
+    if not np.issubdtype(dataframe_with_outcome[outcome_column_label].dtype, np.number):
+        raise ValueError(f"{outcome_column_label} is not numeric. Please confirm outcome_column_label is the correct column or convert the column in the dataframe to numeric.")
+    else:
+        # Make a copy of the dataframe to work on
+        dataframe_with_standardized_outcome = dataframe_with_outcome.copy()
+
+
+        if convert_to_years:
+            # Create a copy of the outcome column and convert it to years
+            dataframe_with_standardized_outcome = convertDaysToYears(dataframe_with_standardized_outcome, outcome_column_label)
+
+            # Rename the converted column with the standardized name
+            dataframe_with_standardized_outcome.rename(columns={f"{outcome_column_label}_years": standard_column_label}, inplace=True)
+        else:
+
+            # Make a copy of the outcome column with the standardized name
+            dataframe_with_standardized_outcome[standard_column_label] = dataframe_with_standardized_outcome[outcome_column_label]
+    
+    return dataframe_with_standardized_outcome
