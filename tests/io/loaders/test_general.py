e1aa3d8cda8d2c61f50b23e81a15ad307c6f98c0
Katy Scott <k.l.scott16@gmail.com>
2024-12-13 11:55:31 -0500

    refactor: made expected image types a fixture

--- tests/io/loaders/test_general.py
+++ tests/io/loaders/test_general.py
@@ -9,16 +9,21 @@ def nsclcConfigDirPath():
 def lung4DConfigDirPath():
     return "tests/4D-Lung"
 
-def test_NSCLC_loadImageDatasetConfig(nsclcConfigDirPath):
+@pytest.fixture
+def expected_image_types():
+    return ["original", "shuffled_full","shuffled_roi","shuffled_non_roi","randomized_sampled_full","randomized_sampled_roi","randomized_sampled_non_roi"]
+
+
+def test_NSCLC_loadImageDatasetConfig(nsclcConfigDirPath, expected_image_types):
     config = loadImageDatasetConfig("NSCLC_Radiogenomics", nsclcConfigDirPath)
     assert config["dataset_name"] == "NSCLC_Radiogenomics"
-    assert config["image_types"] == ["original", "shuffled_full","shuffled_roi","shuffled_non_roi","randomized_sampled_full","randomized_sampled_roi","randomized_sampled_non_roi"]
+    assert config["image_types"] == expected_image_types
     assert config["outcome_variables"]["event_label"] == "Survival Status"
     assert config["outcome_variables"]["event_value_mapping"] == {'Alive': 0, 'Dead': 1}
 
-def test_lung4D_loadImageDatasetConfig(lung4DConfigDirPath):
+def test_lung4D_loadImageDatasetConfig(lung4DConfigDirPath, expected_image_types):
     config = loadImageDatasetConfig("4D-Lung", lung4DConfigDirPath)
     assert config["dataset_name"] == "4D-Lung"
-    assert config["image_types"] == ["original", "shuffled_full","shuffled_roi","shuffled_non_roi","randomized_sampled_full","randomized_sampled_roi","randomized_sampled_non_roi"]
-    assert config["outcome_variables"]["event_label"] == None
-    assert config["outcome_variables"]["event_value_mapping"] == None
+    assert config["image_types"] == expected_image_types
+    assert config["outcome_variables"]["event_label"] is None
+    assert config["outcome_variables"]["event_value_mapping"] is None
