6370597f5e8ca42944aa6a6eb0284481dc208ff6
Katy Scott <k.l.scott16@gmail.com>
2024-12-16 12:02:27 -0500

    feat: add function for plotting correlation distribution as a histogram

--- src/readii/analyze/plot_correlation.py
+++ src/readii/analyze/plot_correlation.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
+from scipy.linalg import issymmetric
 
 from readii.utils import logger
 
@@ -90,3 +91,80 @@ def plotCorrelationHeatmap(correlation_matrix_df:pd.DataFrame,
     plt.suptitle(title, fontsize=14)
     
     return corr_fig
+
+
+
+def plotCorrelationHistogram(correlation_matrix:pd.DataFrame,
+                             num_bins:int = 100,
+                             xlabel:Optional[str] = "Correlations",
+                             ylabel:Optional[str] = "Frequency",
+                             y_lower_bound:int = 0,
+                             y_upper_bound:Optional[int] = None,
+                             title:Optional[str] = "Distribution of Correlations for Features",
+                             subtitle:Optional[str] = "",
+                             ):
+    """ Function to plot a distribution of correlation values for a correlation matrix.
+
+    Parameters
+    ----------
+    correlation_matrix : pd.DataFrame
+        Dataframe containing the correlation matrix to plot.
+    num_bins : int, optional
+        Number of bins to use for the distribution plot. The default is 100.
+    xlabel : str, optional
+        Label for the x-axis. The default is "Correlations".
+    ylabel : str, optional
+        Label for the y-axis. The default is "Frequency".
+    y_lower_bound : int, optional
+        Lower bound for the y-axis of the distribution plot. The default is 0.
+    y_upper_bound : int, optional
+        Upper bound for the y-axis of the distribution plot. The default is None.
+    title : str, optional
+        Title for the plot. The default is "Distribution of Correlations for Features".
+    subtitle : str, optional
+        Subtitle for the plot. The default is "".
+
+    Returns
+    -------
+    dist_fig : plt.Figure
+        Figure object containing the histogram of correlation values.
+    bin_values : np.ndarray or list of arrays
+        Numpy array containing the values in each bin for the histogram.
+    bin_edges : np.ndarray
+        Numpy array containing the bin edges for the histogram.
+    """
+    
+    # Convert to numpy to use histogram function
+    feature_correlation_arr = correlation_matrix.to_numpy()
+
+    # Check if matrix is symmetric
+    if issymmetric(feature_correlation_arr):
+        print("Correlation matrix is symmetric.")
+        # Get only the bottom left triangle of the correlation matrix since the matrix is symmetric 
+        lower_half_idx = np.mask_indices(feature_correlation_arr.shape[0], np.tril)
+        # This is a 1D array for binning and plotting
+        correlation_vals = feature_correlation_arr[lower_half_idx]
+    else:
+        # Flatten the matrix to a 1D array for binning and plotting
+        correlation_vals = feature_correlation_arr.flatten()
+
+    # Set up figure and axes for the plot
+    dist_fig, dist_ax = plt.subplots()
+
+    # Plot the histogram of correlation values
+    bin_values, bin_edges, _ = dist_ax.hist(correlation_vals, bins=num_bins)
+
+    # Set up axis labels
+    dist_ax.set_xlabel(xlabel)
+    dist_ax.set_ylabel(ylabel)
+
+    # Set axis bounds
+    dist_ax.set_xbound(-1.0, 1.0)
+    dist_ax.set_ybound(y_lower_bound, y_upper_bound)
+
+    # Set title and subtitle
+    # Suptitle is the super title, which will be above the title
+    plt.suptitle(title, fontsize=14)
+    plt.title(subtitle, fontsize=10)
+
+    return dist_fig, bin_values, bin_edges
