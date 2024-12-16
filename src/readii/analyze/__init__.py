0f9f3318629414fd9ed5a019573549eef0aeb175
Katy Scott <k.l.scott16@gmail.com>
2024-12-16 12:02:58 -0500

    feat: add correlation plot functions to init

--- src/readii/analyze/__init__.py
+++ src/readii/analyze/__init__.py
@@ -1,10 +1,14 @@
 """ Module to perform analysis on READII outputs """
 
 from .correlation import getFeatureCorrelations, getVerticalSelfCorrelations, getHorizontalSelfCorrelations, getCrossCorrelationMatrix
+from.plot_correlation import plotCorrelationHeatmap, plotCorrelationHistogram
+
 
 __all__ = [
     'getFeatureCorrelations',
     'getVerticalSelfCorrelations',
     'getHorizontalSelfCorrelations',
-    'getCrossCorrelationMatrix'
+    'getCrossCorrelationMatrix',
+    'plotCorrelationHeatmap',
+    'plotCorrelationHistogram'
 ]
