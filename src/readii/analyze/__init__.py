de87bef50955298f9bcabba42a65dfa7c92d1e1f
Katy Scott <k.l.scott16@gmail.com>
2024-12-16 14:37:10 -0500

    style: add space in import statement for plot correlation

--- src/readii/analyze/__init__.py
+++ src/readii/analyze/__init__.py
@@ -1,7 +1,7 @@
 """ Module to perform analysis on READII outputs """
 
 from .correlation import getFeatureCorrelations, getVerticalSelfCorrelations, getHorizontalSelfCorrelations, getCrossCorrelationMatrix
-from.plot_correlation import plotCorrelationHeatmap, plotCorrelationHistogram
+from .plot_correlation import plotCorrelationHeatmap, plotCorrelationHistogram
 
 
 __all__ = [
