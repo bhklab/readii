d61e1f4e6e66ad5ca6b372b303162cf155fde501
Katy Scott <k.l.scott16@gmail.com>
2025-01-17 17:25:29 -0500

    feat: add cropToCentroid function and validateNewDimensions function for quick checking dimension match between image and centroid/mask/dimensions/etc.

--- src/readii/process/images/crop.py
+++ src/readii/process/images/crop.py
@@ -6,6 +6,37 @@ from imgtools.ops.functional import resample
 from readii.utils import logger
 
 
+def validateNewDimensions(image:sitk.Image,
+                          new_dimensions:tuple
+                          ) -> None:
+    """Validate that the input new dimensions are valid for the image.
+
+    Parameters
+    ----------
+    image : sitk.Image
+        Image to validate the new dimensions for.
+    new_dimensions : tuple
+        Tuple of integers representing the new dimensions to validate.
+
+    Raises
+    ------
+    ValueError
+        If the new dimensions are not valid for the image.
+    """
+    # Check that the number of dimensions in the new dimensions matches the number of dimensions in the image
+    if len(new_dimensions) != image.GetDimension():
+        msg = f"Number of dimensions in new_dimensions ({len(new_dimensions)}) does not match the number of dimensions in the image ({image.GetDimension()})."
+        logger.exception(msg)
+        raise ValueError(msg)
+    
+    # Check that the new dimensions are integers
+    if not all(isinstance(dim, int) for dim in new_dimensions):
+        msg = "New dimensions must be integers."
+        logger.exception(msg)
+        raise ValueError(msg)
+
+
+
 def resizeImage(image:sitk.Image,
                 resized_dimensions:tuple
                 ) -> sitk.Image:
@@ -45,6 +76,7 @@ def resizeImage(image:sitk.Image,
 
     return resized_image
     
+
 
 def findBoundingBox(mask:sitk.Image,
                     min_dim_size:int = 4) -> np.ndarray:
@@ -108,3 +140,60 @@ def findCentroid(mask:sitk.Image) -> np.ndarray:
     # Convert the physical point to an index in the mask array
     centroid_idx = mask.TransformPhysicalPointToIndex(centroid_coords)
     return np.asarray(centroid_idx, dtype=np.float32)
+
+
+
+def cropToCentroid(image:sitk.Image,
+                   centroid:tuple,
+                   crop_dimensions:tuple,
+                   ) -> sitk.Image:
+    """Crop an image centered on the centroid with specified crop dimension. No resizing/resampling is performed.
+
+    Parameters
+    ----------
+    image : sitk.Image
+        Image to crop.
+    centroid : tuple
+        Tuple of integers representing the centroid of the image to crop. Must have the same number of dimensions as the image.
+    crop_dimensions : tuple
+        Tuple of integers representing the dimensions to crop the image to. Must have the same number of dimensions as the image.
+
+    Returns
+    -------
+    cropped_image : sitk.Image
+        Cropped image.
+    """
+    # Check that the number of dimensions in the crop dimensions matches the number of dimensions in the image
+    validateNewDimensions(image, crop_dimensions)
+
+    # Check that the centroid dimensions match the image dimensions
+    validateNewDimensions(image, centroid)
+
+    min_x = int(centroid[0] - crop_dimensions[0] // 2)
+    max_x = int(centroid[0] + crop_dimensions[0] // 2)
+    min_y = int(centroid[1] - crop_dimensions[1] // 2)
+    max_y = int(centroid[1] + crop_dimensions[1] // 2)
+    min_z = int(centroid[2] - crop_dimensions[2] // 2)
+    max_z = int(centroid[2] + crop_dimensions[2] // 2)
+
+
+    img_x, img_y, img_z = image.GetSize()
+
+    
+    if min_x < 0:
+        min_x, max_x = 0, crop_dimensions[0]
+    elif max_x > img_x:
+        min_x, max_x = img_x - crop_dimensions[0], img_x
+
+    if min_y < 0:
+        min_y, max_y = 0, crop_dimensions[1]
+    elif max_y > img_y:
+        min_y, max_y = img_y - crop_dimensions[1], img_y
+
+    if min_z < 0:
+        min_z, max_z = 0, crop_dimensions[2]
+    elif max_z > img_z:
+        min_z, max_z = img_z - crop_dimensions[2], img_z
+
+    return image[min_x:max_x, min_y:max_y, min_z:max_z]
+
