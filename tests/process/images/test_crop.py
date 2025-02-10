1d2561b3378d62b37a37b96b9651733b9dac1db9
Katy Scott <k.l.scott16@gmail.com>
2025-02-10 17:10:18 -0500

    test: update to crop testing for new function

--- tests/process/images/test_crop.py
+++ tests/process/images/test_crop.py
@@ -3,17 +3,8 @@ import SimpleITK as sitk
 
 from readii.image_processing import loadDicomSITK, loadSegmentation
 from readii.process.images.crop import (
-    apply_bounding_box_limits,
-    check_bounding_box_single_dimension,
-    crop_image_to_mask,
-    crop_to_bounding_box,
-    crop_to_centroid,
-    crop_to_maxdim_cube,
-    find_bounding_box,
-    find_centroid,
-    validate_new_dimensions,
+    crop_and_resize_image_and_mask
 )
-
 
 @pytest.fixture
 def nsclcCT():
@@ -51,28 +42,40 @@ def lung4D_mask(lung4D_ct_path, lung4D_rt_path):
     return segDictionary["Tumor_c40"]
 
 
+def test_default_crop_and_resize_image(lung4D_image, lung4D_mask):
+    cropped_image, cropped_mask = crop_and_resize_image_and_mask(lung4D_image, lung4D_mask)
+    assert cropped_image.GetSize() == (50, 50, 50), \
+        f"Cropped image size is incorrect, expected {(50, 50, 50)}, got {cropped_image.GetSize()}"
+    assert cropped_mask.GetSize() == (50, 50, 50), \
+        f"Cropped mask size is incorrect, expected {(50, 50, 50)}, got {cropped_mask.GetSize()}"
+
 @pytest.mark.parametrize(
-    "crop_method, expected_size",
+    "crop_method, resize_dimnesion, expected_size",
     [
-        ("bounding_box", (50, 50, 50)),
-        ("centroid", (50, 50, 50)),
-        ("cube", (50, 50, 50)),
-        # ("pyradiomics", (22, 28, 14)),
+        ("bounding_box", None, (51, 92, 28)),
+        ("centroid", None, (50, 50, 50)),
+        ("cube", None, (92, 92, 92)),
+        ("bounding_box", (50, 50, 50), (50, 50, 50)),
+        ("centroid", (50, 50, 50), (50, 50, 50)),
+        ("cube", (50, 50, 50), (50, 50, 50)),
+        ("bounding_box", (49, 49, 49), (49, 49, 49)),
+        ("centroid", (49, 49, 49), (49, 49, 49)),
+        ("cube", (49, 49, 49), (49, 49, 49)),
     ],
 )
-def test_crop_image_to_mask_methods(
+def test_crop_and_resize_image_and_mask_methods_and_resize_dimension(
     lung4D_image,
     lung4D_mask,
     crop_method,
+    resize_dimension
     expected_size,
-    resize_dimensions=(50, 50, 50),
 ):
     """Test cropping image to mask with different methods"""
-    cropped_image, cropped_mask = crop_image_to_mask(
+    cropped_image, cropped_mask = crop_and_resize_image_and_mask(
         lung4D_image,
         lung4D_mask,
-        crop_method,
-        resize_dimensions,
+        crop_method = crop_method,
+        resize_dimension = resize_dimension,
     )
     assert (
         cropped_image.GetSize() == expected_size
@@ -82,96 +85,29 @@ def test_crop_image_to_mask_methods(
     ), f"Cropped mask size is incorrect, expected {expected_size}, got {cropped_mask.GetSize()}"
 
 
+
 ####################################################################################################
 # Parameterized tests for find_centroid and find_bounding_box
 
 
-@pytest.fixture
-def image_and_mask_with_roi(request, label_value: int = 1):
-    """
-    Fixture to create a 3D image and mask with a specified region of interest (ROI).
+# @pytest.fixture
+# def image_and_mask_with_roi(request, label_value: int = 1):
+#     """
+#     Fixture to create a 3D image and mask with a specified region of interest (ROI).
 
-    This fixture is used indirectly in parameterized tests via the `indirect` keyword
-    in `pytest.mark.parametrize`. The `request.param` provides the ROI coordinates, which
-    are used to define the region of interest in the mask. The ROI is specified as a
-    6-tuple (x_min, x_max, y_min, y_max, z_min, z_max).
+#     This fixture is used indirectly in parameterized tests via the `indirect` keyword
+#     in `pytest.mark.parametrize`. The `request.param` provides the ROI coordinates, which
+#     are used to define the region of interest in the mask. The ROI is specified as a
+#     6-tuple (x_min, x_max, y_min, y_max, z_min, z_max).
 
-    """
-    # Create a 3D image
-    image = sitk.Image(100, 100, 100, sitk.sitkInt16)
-    mask = sitk.Image(100, 100, 100, sitk.sitkUInt8)
+#     """
+#     # Create a 3D image
+#     image = sitk.Image(100, 100, 100, sitk.sitkInt16)
+#     mask = sitk.Image(100, 100, 100, sitk.sitkUInt8)
 
-    # Unpack ROI from the request parameter and apply it to the mask
-    roi = request.param
-    mask[roi[0] : roi[1], roi[2] : roi[3], roi[4] : roi[5]] = label_value
+#     # Unpack ROI from the request parameter and apply it to the mask
+#     roi = request.param
+#     mask[roi[0] : roi[1], roi[2] : roi[3], roi[4] : roi[5]] = label_value
 
-    return image, mask
+#     return image, mask
 
-
-def test_find_bounding_box_and_centroid_bad_label():
-    mask = sitk.Image(100, 100, 100, sitk.sitkUInt8)
-    mask[10:20, 10:20, 10:20] = 2
-
-    with pytest.raises(RuntimeError):
-        find_bounding_box(mask)
-
-    with pytest.raises(RuntimeError):
-        find_centroid(mask)
-
-
-# Test cases for find_bounding_box
-
-
-@pytest.mark.parametrize(
-    # First parameter passed indirectly via the fixture
-    # Second parameter is the expected bounding box
-    "image_and_mask_with_roi, expected_bbox",
-    [
-        #
-        # x_min, x_max, y_min, y_max, z_min, z_max
-        #
-        # Simple case: Perfect bounding box
-        ((85, 95, 70, 90, 60, 90), (85, 95, 70, 90, 60, 90)),
-        # Complex case: Non-standard bounding box dimensions
-        ((32, 68, 53, 77, 10, 48), (32, 68, 53, 77, 10, 48)),
-        # Single-plane ROI: ROI in only one slice
-        # since min_dim_size is 4, the ROI is expanded to 4 in if a side is too small
-        # x_max is expanded to 24
-        ((20, 21, 30, 60, 40, 80), (20, 24, 30, 60, 40, 80)),
-        # Minimum size ROI
-        # x_max is expanded to 49, y_max is expanded to 14, z_max is expanded to 9
-        ((45, 46, 10, 12, 5, 6), (45, 49, 10, 14, 5, 9)),
-    ],
-    indirect=["image_and_mask_with_roi"],  # Use the fixture indirectly
-)
-def test_find_bounding_box(image_and_mask_with_roi, expected_bbox):
-    _, mask = image_and_mask_with_roi
-    bounding_box = find_bounding_box(mask, min_dim_size=4)
-    assert (
-        bounding_box == expected_bbox
-    ), f"Bounding box is incorrect, expected {expected_bbox}, got {bounding_box}"
-
-
-# Test cases for find_centroid
-
-
-@pytest.mark.parametrize(
-    "image_and_mask_with_roi, expected_centroid",
-    [
-        # Simple case: Perfect centroid
-        ((85, 95, 70, 90, 60, 90), (90, 80, 75)),
-        # Complex case: Non-standard dimensions
-        ((32, 68, 53, 77, 10, 48), (50, 65, 29)),
-        # Single-plane ROI
-        ((20, 21, 30, 60, 40, 80), (20, 45, 60)),
-        # Minimum size ROI
-        ((45, 46, 10, 12, 5, 6), (45, 11, 5)),
-    ],
-    indirect=["image_and_mask_with_roi"],  # Use the fixture indirectly
-)
-def test_find_centroid(image_and_mask_with_roi, expected_centroid):
-    _, mask = image_and_mask_with_roi
-    centroid = find_centroid(mask)
-    assert (
-        centroid == expected_centroid
-    ), f"Centroid is incorrect, expected {expected_centroid}, got {centroid}"
