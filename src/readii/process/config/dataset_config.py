239b3818424ce887c7fca11faf487d06baa41ff4
Katy Scott <k.l.scott16@gmail.com>
2025-05-26 11:59:14 -0400

    fix: ruff fixes

--- src/readii/process/config/dataset_config.py
+++ src/readii/process/config/dataset_config.py
@@ -4,9 +4,9 @@ from typing import Dict, Union
 from readii.io.loaders.general import loadImageDatasetConfig
 from readii.utils import logger
 
+
 def get_full_data_name(config: Union[Dict | Path]):
     """Combine DATA_SOURCE and DATASET_NAME config variables into a single string."""
-    
     match type(config):
         case Path():
             dataset_config = loadImageDatasetConfig(config.stem, config.parent)
